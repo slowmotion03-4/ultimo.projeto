82f5e1e7e65b43cf58d2a591aae6aae02bf77afb
slowmotion03-4 <ritarosa995@gmail.com>
2024-11-20 22:08:51 -0300

    ;)

---
+++ perguntas.js
@@ -0,0 +1,58 @@
+criaCartao(
+    'MODA',
+    'como a moda sustentável está mudando o mundo?',
+    ' transformação do setor têxtil e no combate aos impactos ambientais e sociais da indústria da moda'
+)
+
+criaCartao(
+    'CHANEL',
+    'Quem foi a criadora da marca?',
+    'Coco Chanel/Gabrielle Bonheur'
+)
+
+criaCartao(
+    'MODA',
+    'como a moda reflete na cultura de uma sociedade?',
+    'sendo uma forma de expressão de identidade pessoal e coletiva, além de simbolizar as mudanças sociais, políticas e econômicas. Ela pode representar status social, influenciando a percepção de classe e refletindo as transformações tecnológicas. '
+)
+
+criaCartao(
+    'expressões utilizada na moda ',
+    'o que significa Fast fashion?',
+    'são roupas produzidas  rapidamente e a baixo custo '
+)
+
+criaCartao(
+'Givenchy',
+'Quando foi criada a marca',
+'1952 na frança'
+
+)
+
+criaCartao(
+'It girls',
+'As maiores it girls da geração z',
+' Olivia rodrigo, Billie Eillish, Bella hadid, Kylie jenner e Gigi hadid'
+
+)
+
+criaCartao(
+'MÚSICA',
+'Qual é o albúm mais escutado do spotfy',
+'Sour by Olivia rodrigo'
+
+)
+
+criaCartao(
+'MÚSICA',
+'Qual foi o primeiro EP do the weenked',
+'House of Balloons'
+
+)
+
+criaCartao(
+'Música',
+'Qual foi a artista que ganhou mais grammy em 2020',
+'Billie Eillish'
+
+)
